da6bde60b2c7e4a95ad840488fb8813ba62c3fda
Salvador Borit <salvaborit@gmail.com>
2022-11-27 17:24:36 -0300

    .

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Module for testing State model"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
@@ -7,13 +7,10 @@ from models.state import State
 class test_state(test_basemodel):
     """ """
 
-    def __init__(self, *args, **kwargs):
+    def setUp(self):
         """ """
-        super().__init__(*args, **kwargs)
-        self.name = "State"
-        self.value = State
+        self.state = State(name='Florida')
 
     def test_name3(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(self.state.name), str)
