9e0ecb98274b6b77b1d63d772051e19b1da30798
blas <blashernandez98@gmail.com>
2022-11-27 22:19:45 -0300

    Added flag to remove other warning

--- models/place.py
+++ models/place.py
@@ -36,7 +36,8 @@ class Place(BaseModel, Base):
         reviews = relationship(
             'Review', cascade='all, delete', backref='place')
         amenities = relationship(
-            'Amenity', secondary='place_amenity', viewonly=False)
+            'Amenity', secondary='place_amenity',
+            viewonly=False, overlaps="place_amenities")
     else:
         @property
         def reviews(self):
