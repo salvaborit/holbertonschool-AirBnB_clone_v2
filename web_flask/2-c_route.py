39318dc490d9bed05b10970292e37452dbc22f1b
Salvador Borit <salvaborit@gmail.com>
2022-11-28 11:37:04 -0300

    .

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Task 2
-"""
+"""Task 2"""
 from flask import Flask
 
 
@@ -10,16 +8,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """Displays 'Hello HBNB!' on route '/'"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB' on route '/hbnb'"""
     return 'HBNB'
 
 
 @app.route(f'/c/<text>', strict_slashes=False)
 def c(text):
+    """Displays 'C <text>' on route '/c/<text>'"""
     return 'C ' + str(text)
 
 
