59d52d1e1e99783d809814fe80ea67c82a4a66e3
Salvador Borit <salvaborit@gmail.com>
2022-11-28 12:47:15 -0300

    .

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Task 3"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """Displays 'Hello HBNB!' on route '/'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays 'HBNB' on route '/hbnb'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """Displays 'C' followed by val of 'text' var on route '/c/<text>'"""
+    return 'C ' + str(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """Displays 'Python' followed by val of 'text' var on route '/python/<text>'"""
+    return 'Python ' + str(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
