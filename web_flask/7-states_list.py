5b545ad2778ac1cbc5f46232fe590799eb272b21
Salvador Borit <salvaborit@gmail.com>
2022-11-30 10:50:47 -0300

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 
 
 from flask import Flask, render_template
+from models import storage
 
 
 app = Flask(__name__)
@@ -12,7 +13,6 @@ app.url_map.strict_slashes = False
 @app.route('/states_list')
 def states_list():
     """ List of states """
-    from models import storage
     from models.state import State
 
     states = storage.all(State)
@@ -22,8 +22,7 @@ def states_list():
 @app.teardown_appcontext
 def teardown_db(exception):
     """ Removes SQLAlchemy session after every request """
-    from models import storage
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
