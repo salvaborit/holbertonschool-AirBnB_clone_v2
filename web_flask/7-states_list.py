88e3c3ca4e671fc628ef11c39b0e66c348a479a5
Salvador Borit <salvaborit@gmail.com>
2022-11-30 10:40:07 -0300

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,14 +8,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/states_list')
 def states_list():
     """ List of states """
+    from models import storage
     from models.state import State
 
-    from models import storage
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
