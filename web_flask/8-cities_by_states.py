152a654ff26af5a9e747114c8571e8205a0a1991
Salvador Borit <salvaborit@gmail.com>
2022-11-30 19:23:16 -0300

    .

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
-"""MODULE NAME"""
+""" Module 7-states_list.py """
+from flask import Flask
 
 
+app = Flask(__name__)
 
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ remove the current SQLAlchemy Session """
+    from models import storage
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Renders all cities and states """
+    from models import storage
+    from models.state import State
+    from models.city import City
+    from flask import render_template
+
+    return render_template('7-states_list.html',
+                           state_list=storage.all(State).values(),
+                           city_list=storage.all(City).values())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
